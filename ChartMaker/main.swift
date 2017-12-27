43dca13e6697b6f4d267a44c40e812543b247f3e
Charlie Williams <c@charliewilliams.org>
2017-12-27 12:49:01 -0700

    Rename / add second target for charts making

---
+++ ChartMaker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ChartMaker
+//
+//  Created by Charlie Williams on 27/12/2017.
+//  Copyright © 2017 Charlie Williams. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
