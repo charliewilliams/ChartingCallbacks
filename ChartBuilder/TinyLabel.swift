5941485a8793f8ff836365dcf03d939e60891af7
Charlie Williams <c@charliewilliams.org>
2018-01-13 21:03:50 +0000

    Try speeding up drawing

--- ChartBuilder/TinyLabel.swift
+++ ChartBuilder/TinyLabel.swift
@@ -24,6 +24,7 @@ class TinyLabel: NSTextView {
         frameRotation = 90
         alignment = .right
         isSelectable = false
+        drawsBackground = true
     }
 
     override init(frame frameRect: NSRect, textContainer container: NSTextContainer?) {
@@ -36,5 +37,16 @@ class TinyLabel: NSTextView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func draw(_ dirtyRect: NSRect) {
+
+        if let context = NSGraphicsContext.current {
+            context.shouldAntialias = false
+            context.cgContext.setShouldAntialias(true)
+            context.cgContext.setShouldSmoothFonts(false)
+        }
+
+        super.draw(dirtyRect)
     }
 }
